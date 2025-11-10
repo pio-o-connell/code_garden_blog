f31c0491f21059c2f3355b8e2050bfda0a2607cf
pio-o-connell <pio_o_con@hotmail.com>
2025-11-10 11:19:09 +0000

    Build & Deploy post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("title", "author", "created_on", "status")
+	prepopulated_fields = {"slug": ("title",)}
+	search_fields = ("title", "content")
+	list_filter = ("status", "created_on")
