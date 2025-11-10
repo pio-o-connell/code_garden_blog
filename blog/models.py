0ee3f6764d606fa135145ec729459e3b9722d4d8
pio-o-connell <pio_o_con@hotmail.com>
2025-11-10 12:52:43 +0000

    Create comment model

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,11 @@ class Post(models.Model):
         return f"{self.title} ({'Published' if self.status==1 else 'Draft'})"
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
