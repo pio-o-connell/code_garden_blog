6776b812a2422ed736d96be6a563a71e55624e55
pio-o-connell <pio_o_con@hotmail.com>
2025-11-10 13:29:00 +0000

    add comment fix

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 @admin.register(Post)
@@ -8,3 +8,10 @@ class PostAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("title",)}
 	search_fields = ("title", "content")
 	list_filter = ("status", "created_on")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ("post", "author", "created_on", "approved")
+	search_fields = ("body",)
+	list_filter = ("approved", "created_on")
