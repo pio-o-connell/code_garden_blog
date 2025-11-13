27b8fb80a3fae041aedc272e7239e6ece2fa282c
pio-o-connell <pio_o_con@hotmail.com>
2025-11-13 12:20:15 +0000

    Add About app

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import About
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+    readonly_fields = ('image_preview',)
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'content', 'image', 'image_preview')
+        }),
+    )
+
+    def image_preview(self, obj):
+        if obj and getattr(obj, 'image'):
+            return f"<img src='{obj.image.url}' style='max-width:200px; height:auto;' />"
+        return "(No image)"
+    image_preview.allow_tags = True
+    image_preview.short_description = 'Image preview'
--- code_garden/urls.py
+++ code_garden/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),  
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
