0b9ec1ad8cd6c401629ff1e0fbd9f5b14391302d
pio-o-connell <pio_o_con@hotmail.com>
2025-11-17 15:26:23 +0000

    Add form view tests

---
+++ blog/test_forms.py
@@ -0,0 +1,18 @@
+
+
+# Create your tests here.
+from django.test import TestCase
+from .forms import CommentForm
+
+
+class TestCommentForm(TestCase):
+
+    def test_form_is_valid(self):
+        # comment_form = CommentForm({'body': 'This is a great post'})
+        comment_form = CommentForm({'body': 'This is a great post'})
+        # comment_form = CommentForm({'body': ''})
+        self.assertTrue(comment_form.is_valid())
+
+    def test_form_is_invalid(self):
+        comment_form = CommentForm({'body': ''})
+        self.assertFalse(comment_form.is_valid(), msg="Form is valid")
